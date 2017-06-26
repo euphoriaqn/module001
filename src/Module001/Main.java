fede57fe2fbfea8de7fc62d5bd33da53f7708b3c
euphoriaqn <euphoriaqn@gmail.com>
2017-06-26 23:57:47 +0300

    commit all

---
+++ src/Module001/Main.java
@@ -0,0 +1,10 @@
+package Module001;
+
+/**
+ * Created by Илья on 26.06.2017.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Лето 2017 года я проведу просто замечательно!!GoJava!!!!");
+    }
+}
